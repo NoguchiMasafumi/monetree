96dc4b8055ee07ce30a66fc6b7e263ba82b29094
NoguchiMasafumi <89947969+NoguchiMasafumi@users.noreply.github.com>
2022-02-21 12:30:19 +0900

    Add files via upload

---
+++ js/contents.js
@@ -0,0 +1,79 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
+
+
+var app=new Vue({
+  el:"#title1",
+  data:{
+    site_name:'Tribe Factory'
+  }
+})
+
+var app=new Vue({
+  el:"#head1",
+  data:{
+    site_name:'Tribe Factory'
+  }
+})
+
+
+
+
+
+
+
+var value = new Vue({
+  el: '#app-blog',
+  data: function() {
+    return({
+      items:[
+        {
+          url: 'blog/19990314.htm',
+          text: '初心者が戸惑う証券会社',
+          img_url:'',
+          explain:'資産を増やしたいと思った時ネットなどで調べると、一番多く出てくる方法は「株をやってみては？」という提案です。しかしながら株をやるといってもどうやればいいかわからない。まず最初に誰もがつまづくところです。そしてこの壁がほとんどの方が超えられず「まぁいいか」で挫折してしまいます。'
+        },
+        {
+          url:'blog/20161006.htm',
+          text:'2016-10-06 ニュージーランドツーリング',
+          img_url:'https://lh3.googleusercontent.com/WDNf68MrMo0Qr4WftMatueeSCcU5N4AK5PfqzGRSehdGbQ3p_1MzrKIJUZj8YlSnAj0J6mAC7GaTP6vqPkhhhT3WvE9eSfVIpx77Clqk75Yj4Bh1bdifxvqsLICV-Bk_5STTVxm0HQn-W-KMdFm3gboPt7t3AqKpw1l5zCMtZJ2EqNvY7kUxL95h5OMAAyfsEBBmARNhZS_Ryitov2qieMAXw0OjVB0Wj6rIDljIaCH8BwVwZjqxhir0un3TmJgpCFzemtAQNjMjpRHGKyqtcGgo8x-vN1cti9VtvowTmsmNEXImoR0PxKYlMb5r-syMpGh8gYageN4AgIPVlj0z1cduL4vOTRlGSxfsEVZJUY0FaM-YCR3NJJhVM1DzjcGQ9oXRjYjuHNgIXLaMVyYw4NlNMloLxywrryYPGB0wGkdgbiUcli7y5BekEN8qvAN-dF1OyjrAAiQ_-O2RmnOdUc0XBl9rGxyYk1qe26R5-lzIaDrHyZnZUCmfg-OiO5bEgvhVvXJxeRwnfirZ3yrhRQ_PGy1uCi2SDiA9Himu2wB8RTZ_r8S94G1W5Zdj8nwo7sPXEzhM87Z23w89A8rVBlQ_1LuNmHqT5mRBKjTesQZ2Kn9vmbtEfiBnI_4oR1riN4lcbm7cHmg8w-fwiE_58UqW5Hyu9-uEPCiVQ76_nleJ9SIR8lxe6D0iX2oz6OfT3eEzE49DiR0r66mlgbYh_EZuzmZ5a8fPX6TtTGIDAV0McRiSSG3tf-ZpRNR7css6e5ddjMaOT6FW_UnYoa8=w350-no?authuser=0',
+          explain:'子供を連れて宿を押さえない旅行をしてきました'
+
+        },
+        {
+          url:'blog/20220221.htm',
+          text:'アウトプットの体裁',
+          img_url:'',
+          explain:'起承転結をはっきりとし口語、丁寧語、の文体をどうするか\n全体の統一感をもたせること\n起：内容の設定\n承：これから記述する内容を予感させるような句\n転：メインの内容\n結：締めくくり。答え'
+
+        },
+        {
+          url:'blog/20220221.htm',
+          text:'コストコなどの大量販売店はNG',
+          img_url:'',
+          explain:'普段皆さんはどんな買い物をしてるでしょうか？　　　\n転：コストコなどの大量販売店で安いからといって本当に必要ではないものをまとめ買いするのは非常に問題です。安く大量に買うことができたという一瞬の快感があるため忘れがちですが長期視点で考えるとただ単に要らないものを買っただけ。つまりは損をしているのです。'
+
+        }
+      ]
+    });
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
